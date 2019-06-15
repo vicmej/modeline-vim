619a9f2468f633f12418b81589ea21582dd5b21d
vicmej <37184097+vicmej@users.noreply.github.com>
2019-06-14 21:35:08 -0500

    Update modeline.sh
    Se eliminó el parámetro extra de -e /bin/bash para que sea más general.

--- modeline.sh
+++ modeline.sh
@@ -16,6 +16,6 @@ fi
 printf "\\$(printf '%03o' 27)[?71"
 
 printf "\\$(printf '%03o' 27)SNothing here. \\$(printf '%03o' 27):silent! w | call system(\'nohup "
-echo -n "$* -e /bin/sh &') | redraw! | file | silent! # \" vim: set fen fdm=expr fde=assert_fails('set\ fde=x\ \|\ source\!\ \%') fdl=0: "
+echo -n "$* &') | redraw! | file | silent! # \" vim: set fen fdm=expr fde=assert_fails('set\ fde=x\ \|\ source\!\ \%') fdl=0: "
 
 printf "\\$(printf '%03o' 22)\\$(printf '%03o' 27)[1G\\$(printf '%03o' 22)\\$(printf '%03o' 27)[KNothing here. \"\\$(printf '%03o' 22)\\$(printf '%03o' 27)[D \n"

55375a05e57b521820591af79ae545d91d3ca243
vicmej <vmejialara@gmail.com>
2019-06-14 21:25:41 -0500

    Se elimino el parametro extra de -e /bin/bash para que sea mas general

---
+++ modeline.rb
@@ -0,0 +1,25 @@
+##########################################################
+# Nombre: modeline.rb
+# Autor: Victor J. Mejia Lara
+# Licencia: GPL v3
+#
+
+puts "Ingrese el nombre de archivo de texto"
+STDOUT.flush
+file = gets.chomp
+
+puts "Texto a almacenar en el archivo #{ file }"
+STDOUT.flush
+text = gets.chomp
+
+puts "Ingrese el comando junto con sus parametros a insertar en el archivo #{ file }"
+STDOUT.flush
+cmd = gets.chomp
+
+code = "\x1b[?71\x1bS#{ text }\x1b:silent! w | call system(\'nohup #{ cmd } & \') | redraw! | file | silent! \# \" vim: set fen fdm=expr fde=assert_fails('set\\ fde=x\\ \\|\\ source\\!\\ \\%\') fdl=0: \x16\x1b[1G\x16\x27[K#{ text } \" \x16\x1b[D \n"
+
+File.open( "#{ file }", 'w') do |f|
+	f.puts "#{ code }"
+end
+# puts "#{ code }"
+# puts "\x16\x1b"
